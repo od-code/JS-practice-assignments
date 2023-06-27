4cb2f7e9f7559c3c2a53c69dfba069ad036946a7
Derrick Atongo <derrickatongo@gmail.com>
2023-06-27 08:12:46 +0300

    Implemented dice roll functionality

--- Projects/Ping game/script.js
+++ Projects/Ping game/script.js
@@ -10,6 +10,20 @@ const scoreEl1 = document.getElementById('score--1');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const diceEl = document.querySelector('.dice');
+const currentScore0 = document.querySelector('#current--0');
+const currentScore1 = document.querySelector('#current--1');
+
+// Initializing the scores variable
+let score = 0;
+//  Implementing the game logic
+diceEl.classList.add('hidden');
+btnRoll.addEventListener('click', function () {
+  let randomDiceRoll = Math.trunc(Math.random() * 6) + 1;
+  diceEl.src = `dice-${randomDiceRoll}.png`;
+  diceEl.classList.remove('hidden');
+  currentScore0.innerHTML = randomDiceRoll;
+});
 
 // HOW TO PLAY THE GAME MODAL
 btnShowModal.addEventListener('click', function () {
